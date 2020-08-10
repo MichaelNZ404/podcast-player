0f38320b762611f8e348d91d8bd835ab4fb54080
Michael Coleman <michael.coleman404@gmail.com>
2020-08-10 12:28:15 +1000

    mobile responsiveness

--- frontend/src/components/Library.js
+++ frontend/src/components/Library.js
@@ -115,6 +115,32 @@ function GenreStripCard(genre: Genre) {
         speed: 500,
         slidesToShow: SLIDES_TO_SHOW,
         slidesToScroll: Math.ceil(SLIDES_TO_SHOW / 3),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ],
         arrows: true
         };
     return (
